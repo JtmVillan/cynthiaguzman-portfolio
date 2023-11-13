9dfa83c951ff9cd297649bd10e87f2bf21463858
Jared Villaueva <jtvillan@ucsd.edu>
2023-11-13 15:59:28 -0800

    formatted beginning layout up to projects

---
+++ cynthiaguzman-portfolio/src/app/page.tsx
@@ -0,0 +1,203 @@
+// import Image from 'next/image'
+import Link from "next/link";
+// import GetPostMetaData from "../../components/GetPostMetaData";
+// import PostPreview from "../../components/PostPreview";
+
+// export default function Home() {
+
+const hero = {
+  content: (
+    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Cynthia Guzman</h1>
+          <p className="mb-5">Mechanical Engineer</p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  ),
+};
+
+const about = {
+  content: (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src="https://i.imgur.com/gBAboXp.png" className="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <h1 className="text-5xl font-bold">Hey I'm Cynthia!</h1>
+          <p className="py-6">B.S Mechanical Engineering with a Specialization in Robotics and Controls.</p>
+          <p>I'm a recent graduate from UC San Diego.
+          </p>
+          <p>Some of my hobbies include crocheting and roller skating.
+          </p>
+        </div>
+      </div>
+    </div>
+  ),
+};
+
+const experience = {
+    content: (
+      <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+        <li>
+          <div className="timeline-middle">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+          </div>
+          <div className="timeline-start md:text-end mb-10">
+            <time className="font-mono italic">1984</time>
+            <div className="text-lg font-black">First Macintosh computer</div>
+            The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh personal computer. It played a pivotal role in establishing desktop publishing as a general office function. The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed in a beige case with integrated carrying handle; it came with a keyboard and single-button mouse.
+          </div>
+          <hr/>
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+          </div>
+          <div className="timeline-end mb-10">
+            <time className="font-mono italic">1998</time>
+            <div className="text-lg font-black">iMac</div>
+            iMac is a family of all-in-one Mac desktop computers designed and built by Apple Inc. It has been the primary part of Apple's consumer desktop offerings since its debut in August 1998, and has evolved through seven distinct forms
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+          </div>
+          <div className="timeline-start md:text-end mb-10">
+            <time className="font-mono italic">2001</time>
+            <div className="text-lg font-black">iPod</div>
+            The iPod is a discontinued series of portable media players and multi-purpose mobile devices designed and marketed by Apple Inc. The first version was released on October 23, 2001, about 8+1⁄2 months after the Macintosh version of iTunes was released. Apple sold an estimated 450 million iPod products as of 2022. Apple discontinued the iPod product line on May 10, 2022. At over 20 years, the iPod brand is the oldest to be discontinued by Apple
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+          </div>
+          <div className="timeline-end mb-10">
+            <time className="font-mono italic">2007</time>
+            <div className="text-lg font-black">iPhone</div>
+            iPhone is a line of smartphones produced by Apple Inc. that use Apple's own iOS mobile operating system. The first-generation iPhone was announced by then-Apple CEO Steve Jobs on January 9, 2007. Since then, Apple has annually released new iPhone models and iOS updates. As of November 1, 2018, more than 2.2 billion iPhones had been sold. As of 2022, the iPhone accounts for 15.6% of global smartphone market share
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+          </div>
+          <div className="timeline-start md:text-end mb-10">
+            <time className="font-mono italic">2015</time>
+            <div className="text-lg font-black">Apple Watch</div>
+            The Apple Watch is a line of smartwatches produced by Apple Inc. It incorporates fitness tracking, health-oriented capabilities, and wireless telecommunication, and integrates with iOS and other Apple products and services
+          </div>
+        </li>
+      </ul>
+    ),
+};
+
+const projects = {
+  content: (
+    <div className="mx-auto px-6">
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4" >
+
+        <div id="project-1" className="mx-auto">
+          <div className="card w-96 bg-base-100 shadow-xl ">
+            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                Shoes!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">Fashion</div> 
+                <div className="badge badge-outline">Products</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="project-2" className="mx-auto">
+          <div className="card w-96 bg-base-100 shadow-xl ">
+            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                Shoes!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">Fashion</div> 
+                <div className="badge badge-outline">Products</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="project-3" className="mx-auto">
+          <div className="card w-96 bg-base-100 shadow-xl ">
+            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                Shoes!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">Fashion</div> 
+                <div className="badge badge-outline">Products</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="project-4" className="mx-auto">
+          <div className="card w-96 bg-base-100 shadow-xl ">
+            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                Shoes!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">Fashion</div> 
+                <div className="badge badge-outline">Products</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  ),
+};
+
+const HomePage = () => {
+  // const postMetaData = GetPostMetaData();
+  // const postPreviews = postMetaData.map((post) => (
+  //   <PostPreview key={post.slug} {...post} />
+  // ));
+
+  // return <div className="grid grid-cols-1 md:grid-cols-2 gap-4" >{postPreviews}</div>;
+  return (
+    <>
+      <div>{hero.content}</div>
+      <div>{about.content}</div>
+      <div>{experience.content}</div>
+      <div>{projects.content}</div>
+    </>
+    
+  );
+};
+
+export default HomePage;
