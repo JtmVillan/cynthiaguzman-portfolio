929ff65496436c0741e5043f1d65849a6443d681
Jared Villaueva <jtvillan@ucsd.edu>
2024-08-18 21:20:26 -0700

    removed credit from footer and updated year

--- cynthiaguzman-portfolio/src/app/components/footer.tsx
+++ cynthiaguzman-portfolio/src/app/components/footer.tsx
@@ -2,8 +2,7 @@ export const Footer = () => {
     return (
         <footer className="footer flex flex-row justify-between py-4 px-8 bg-neutral text-neutral-content">
             <aside className="flex flex-row text-lg">
-                <p> © 2023-2024 Cynthia Guzman | Designed and developed by 
-                <a href="https://jaredvillanueva.com/" className="ease-in duration-150 decoration-accent hover:text-accent" target="_blank" rel="noopener noreferrer"> Jared Villanueva</a>
+                <p> © 2024 Cynthia Guzman 
                 </p>
             </aside>
         </footer>
